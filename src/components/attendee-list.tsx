85e86af4ace69ffaa3494110458468288be57cd7
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-03 00:39:59 +0200

    added tailwind

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,9 +1,13 @@
-
 //import { IconButton } from "./icon-button"
 
 export function AttendeeList() {
-
-    return (
-    <h1>users</h1>
-    )
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-3 items-center"></div>
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <div>
+        <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"   placeholder="Buscar participante..."/>
+      </div>
+    </div>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,20 @@
 
 //import { NavLink } from "./nav-link"
+import nlwUniteIcon from '../assets/unite-icon.svg'
+
 export function Header () {
 
     return (
- <div>
-   
- </div>
-    )
+ <div   className="flex items-center gap-5 py-2  ">
+
+   <img src={nlwUniteIcon} />
+
+   <nav className=' flex items-center gap-5'>
+    <a href="" className=' font-medium text-sm text-zinc-300'> Eventos</a>
+    <a href="" className=' font-medium text-sm'>Participantes</a>
+   </nav>
+
+</div>
+
+)
 }
