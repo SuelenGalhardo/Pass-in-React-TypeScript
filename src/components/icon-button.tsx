2b5f2b1a05ff5b8406a5f45d64d16f530820f371
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-03 22:39:37 +0200

    added style button

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -10,12 +10,20 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
     return (
       <button
         {...props}
-        className={twMerge(
-          "border border-white/10 rounded-md p-1.5",
-          transparent ? "bg-black/20" : "bg-white/10",
-          props.disabled ? "opacity-50" : null
-        )}
+        //className={twMerge(
+          //"border border-white/10 rounded-md p-1.5",
+          //transparent ? "bg-black/20" : "bg-white/10",
+          //props.disabled ? "opacity-50" : null
+
+          className={twMerge(
+            'border border-white/10 rounded-md p-1.5',
+            transparent ? 'bg-black/20' : 'bg-white/10',
+            props.disabled ? 'opacity-50' : null,
+
+
+          )}
+      
       />
-    );
+      )
   }
 
