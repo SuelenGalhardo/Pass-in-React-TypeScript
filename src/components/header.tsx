585183797bd9c9e3d99116577db42b10749737ee
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-03 14:18:09 +0200

    added confg types svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header () {
     return (
  <div   className="flex items-center gap-5 py-2  ">
 
-   <img src={nlwUniteIcon} />
+   <img src={nlwUniteIcon} alt='Unite Icon'/>
 
    <nav className=' flex items-center gap-5'>
     <a href="" className=' font-medium text-sm text-zinc-300'> Events</a>
